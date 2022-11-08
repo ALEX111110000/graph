fecfc8ec25cc4e7eaa5fa95de3ee528a76151f00
lixuefeng <91258271+ALEX111110000@users.noreply.github.com>
2022-11-08 09:44:29 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -60,15 +60,22 @@ class SEDR(nn.Module):
         super(SEDR, self).__init__()
         self.alpha = 1.0
         self.latent_dim = params.gcn_hidden2+params.feat_hidden2
-
+        
+        '''
         # feature autoencoder
         self.encoder = nn.Sequential()
-        self.encoder.add_module('encoder_fc1', full_block(input_dim, params.feat_hidden1, params.p_drop))  #*
-        self.encoder.add_module('encoder_fc2', full_block(params.feat_hidden1, params.feat_hidden2, params.p_drop)) #*feat_hidden2才是feat_x维度。
+        self.encoder.add_module('encoder_L1', full_block(input_dim, params.feat_hidden1, params.p_drop))  #*
+        self.encoder.add_module('encoder_L2', full_block(params.feat_hidden1, params.feat_hidden2, params.p_drop)) #*feat_hidden2才是feat_x维度。
 
         self.decoder = nn.Sequential()
         self.decoder.add_module('decoder_L0', full_block(self.latent_dim, input_dim, params.p_drop)) #*是X‘解码器
+        '''    
+        self.fc1 = nn.Linear(784, 400, bias = False) # Encoder
+		self.fc2 = nn.Linear(400, 784, bias = False) # Decoder
 
+		self.relu = nn.ReLU()
+		self.sigmoid = nn.Sigmoid()
+        
         # GCN layers
         self.gc1 = GraphConvolution(params.feat_hidden2, params.gcn_hidden1, params.p_drop, act=F.relu)
         self.gc2 = GraphConvolution(params.gcn_hidden1, params.gcn_hidden2, params.p_drop, act=lambda x: x)
@@ -78,11 +85,22 @@ class SEDR(nn.Module):
         # DEC cluster layer
         self.cluster_layer = Parameter(torch.Tensor(params.dec_cluster_n, params.gcn_hidden2+params.feat_hidden2))
         torch.nn.init.xavier_normal_(self.cluster_layer.data)
-
+'''
     def encode(self, x, adj):
         feat_x = self.encoder(x)
         hidden1 = self.gc1(feat_x, adj)
         return self.gc2(hidden1, adj), self.gc3(hidden1, adj), feat_x
+'''
+	def encoder(self, x, adj):
+		feat_x = self.relu(self.fc1(x.view(-1, 784)))
+		return feat_x   #*h1变成feat_x
+        hidden1 = self.gc1(feat_x, adj)
+        return self.gc2(hidden1, adj), self.gc3(hidden1, adj), feat_x
+
+	def decoder(self,z):
+		de_feat = self.sigmoid(self.fc2(z))
+		return de_feat
+    
 
     def reparameterize(self, mu, logvar):
         if self.training:
