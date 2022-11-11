c0290ab303259cebdaf8f7d3f7255b7786b40a13
lixuefeng <91258271+ALEX111110000@users.noreply.github.com>
2022-11-11 11:53:42 +0800

    Update utils_func.py

--- src/utils_func.py
+++ src/utils_func.py
@@ -13,15 +13,6 @@ def mk_dir(input_path):
     return input_path
 
 
-def adata_preprocess(i_adata, min_cells=3, pca_n_comps=300):
-    print('===== Preprocessing Data ')
-    sc.pp.filter_genes(i_adata, min_cells=min_cells)
-    adata_X = sc.pp.normalize_total(i_adata, target_sum=1, exclude_highly_expressed=True, inplace=False)['X']
-    adata_X = sc.pp.scale(adata_X)
-    adata_X = sc.pp.pca(adata_X, n_comps=pca_n_comps)
-    return adata_X
-
-
 def load_ST_file(file_fold, count_file='filtered_feature_bc_matrix.h5', load_images=True, file_Adj=None):
     adata_h5 = sc.read_visium(file_fold, load_images=load_images, count_file=count_file)
     adata_h5.var_names_make_unique()
@@ -43,9 +34,20 @@ def load_ST_file(file_fold, count_file='filtered_feature_bc_matrix.h5', load_ima
         adata_h5.obs = adata_h5.obs.join(positions, how="left")
         adata_h5.obsm['spatial'] = adata_h5.obs[['pxl_row_in_fullres', 'pxl_col_in_fullres']].to_numpy()
         adata_h5.obs.drop(columns=['barcode', 'pxl_row_in_fullres', 'pxl_col_in_fullres'], inplace=True)
-
+        
     print('adata: (' + str(adata_h5.shape[0]) + ', ' + str(adata_h5.shape[1]) + ')')
     return adata_h5
+
+def adata_preprocess(i_adata, min_cells=3, pca_n_comps=300):
+    print('===== Preprocessing Data ')
+    sc.pp.filter_genes(i_adata, min_cells=min_cells)
+    adata_X = sc.pp.normalize_total(i_adata, target_sum=1, exclude_highly_expressed=True, inplace=False)['X']
+    adata_X = sc.pp.scale(adata_X)
+    adata_X = sc.pp.pca(adata_X, n_comps=pca_n_comps)
+    return adata_X
+
+#以上是run DLPFC
+
 
 
 # from scanpy
